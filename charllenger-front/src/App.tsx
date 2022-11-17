4a6e458283a0e8f96e4cebec199f0a8520863da4
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2022-11-16 21:48:58 -0300

    feat: routes login/dashboard/register create

--- charllenger-front/src/App.tsx
+++ charllenger-front/src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter } from "react-router-dom";
+import { Switch, Route } from "react-router";
+
+import { Login } from "./pages/login";
+import { Dashboard } from "./pages/dashboard";
+import { Register } from "./pages/register";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login></Login>
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard></Dashboard>
+        </Route>
+        <Route exact path="/register">
+          <Register></Register>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
